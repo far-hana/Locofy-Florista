f0c5a723b73e31f3b7a0a6cca45eaff30aa01d0d
Farhana Yasmeen <farhanayasmeen07@gmail.com>
2022-10-16 19:46:05 +0530

    Final Build

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://us-central1-clone-d9201.cloudfunctions.net/api",
+  baseURL: "http://127.0.0.1:5001/clone-d9201/us-central1/api",
 });
 
 export default instance;
